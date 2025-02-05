7453c6ecda988207587064d0551d75c4394830e9
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2025-02-05 22:23:15 +0100

    First realtime updates.

--- src/main/java/com/example/sensors/controller/TemperatureController.java
+++ src/main/java/com/example/sensors/controller/TemperatureController.java
@@ -2,6 +2,8 @@ package com.example.sensors.controller;
 
 import com.example.sensors.model.sensor.SensorAsync;
 import com.example.sensors.service.TemperatureService;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,12 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/stream/temperature")
 public record TemperatureController(TemperatureService temperatureService) {
-    @GetMapping("/{centralUnitId}")
-    public Flux<SensorAsync> findAllByCentralUnitId(@PathVariable Long centralUnitId) {
-        return this.temperatureService.findAllByCentralUnitId(centralUnitId);
+    @GetMapping(value = "/{centralUnitId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<SensorAsync>> streamSensorData(@PathVariable Long centralUnitId) {
+        return this.temperatureService.findAllByCentralUnitId(centralUnitId)
+                .map(sensor -> ServerSentEvent.builder(sensor)
+                        .event("sensor-update")
+                        .data(sensor)
+                        .build());
     }
 }
--- src/main/java/com/example/sensors/service/TemperatureService.java
+++ src/main/java/com/example/sensors/service/TemperatureService.java
@@ -1,16 +1,19 @@
 package com.example.sensors.service;
 
-import com.example.sensors.model.sensor.Sensor;
 import com.example.sensors.model.sensor.SensorAsync;
 import com.example.sensors.repository.reactor.SensorRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Service
 public record TemperatureService(
         SensorRepository sensorRepository
 ) {
     public Flux<SensorAsync> findAllByCentralUnitId(Long centralUnitId) {
-        return sensorRepository.findAllByCentralUnitId(centralUnitId);
+        return Flux.interval(Duration.ofSeconds(3))  // Poll the database every 1 second
+                .flatMap(tick -> sensorRepository.findAllByCentralUnitId(centralUnitId));
+        // TODO: try distinctUntilChanged
     }
 }
