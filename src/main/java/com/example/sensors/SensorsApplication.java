67d1bdbb637e78ef34faf0eefb34ca59c8c59318
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2025-02-06 15:46:46 +0100

    Remove comment.

--- src/main/java/com/example/sensors/SensorsApplication.java
+++ src/main/java/com/example/sensors/SensorsApplication.java
@@ -7,16 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class SensorsApplication {
-	/**
-	 * On startup:<br>
-	 *  - Init the central units that are saved in the db and inject the sensors.<br>
-	 *  - The central unit polls the physical sensors every n seconds and updates the state of the
-	 * 	building in the database.<br>
-	 *  - The central unit also fetches expectedValue on Sensor from db every x seconds to
-	 * see if there are changes to adjust the heat flow.<br><br>
-	 * After the initialization, we are able to use the spring boot rest pattern to fetch/update data
-	 * for the client.
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SensorsApplication.class, args);
 	}
